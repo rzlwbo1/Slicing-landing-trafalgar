69f9180dc95378e9450f0c2e54f67d282f1d1677
rzlwbo1 <rizalwb2@gmail.com>
2022-01-05 15:29:28 +0700

    owl carousel done

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    autoWidth: true,
+    margin: 10,
+    items: 1,
+    nav: false,
+    dots:false,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+  });
 });
