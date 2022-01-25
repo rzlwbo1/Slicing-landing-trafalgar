e84df08a87358c3e8452c65ca238d72db6bc3eec
rzlwbo1 <rizalwb2@gmail.com>
2022-01-25 11:42:12 +0700

    implement responsive carousel success

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-    autoWidth: true,
     margin: 10,
     items: 1,
     nav: false,
     dots:false,
-    loop: true,
-    autoplay: false,
-    autoplayTimeout: 3000,
     autoplayHoverPause: true,
+    responsiveClass:true,
+    responsive: {
+      0: {
+        loop: false,
+        autoplay: false,
+      },
+      
+      900: {
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+      }
+    }
   });
 });
